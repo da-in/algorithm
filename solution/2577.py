81721e6d04a29fd9af6cc02bae59c0a08420f964
unknown <talown@naver.com>
2021-04-09 20:36:54 +0900

    update 2577 숫자의 개수

--- solution/2577.py
+++ solution/2577.py
@@ -3,13 +3,5 @@ num = 1
 for i in range(0, 3):
     num = num*int(sys.stdin.readline().rstrip())
 num_list = list(map(int, list(str(num))))
-print(num_list.count(0))
-print(num_list.count(1))
-print(num_list.count(2))
-print(num_list.count(3))
-print(num_list.count(4))
-print(num_list.count(5))
-print(num_list.count(6))
-print(num_list.count(7))
-print(num_list.count(8))
-print(num_list.count(9))
+for i in range(0, 10):
+    print(num_list.count(i))
