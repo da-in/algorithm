c71c054f1eb1c6ab070322905f0306f91ade2864
da-in <talown@naver.com>
2022-07-31 17:59:37 +0900

    [refactor] 직사각형 별 찍기

--- Programmers/직사각형별찍기.js
+++ Programmers/직사각형별찍기.js
@@ -3,10 +3,5 @@ process.stdin.on('data', (data) => {
     const n = data.split(' ');
     const a = Number(n[0]),
         b = Number(n[1]);
-    let i = 0;
-    const star = '*';
-    while (i < b) {
-        console.log(star.repeat(a));
-        i++;
-    }
+    console.log(`${'*'.repeat(a)}\n`.repeat(b));
 });
