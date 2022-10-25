e461186bc195d2a0c0aa22a8fa666bac3975adf7
da-in <talown@naver.com>
2022-10-25 14:28:52 +0900

    :memo: Update BOJ JS 25304
    Update BOJ JS 25304

--- BOJ_JS/25304.js
+++ BOJ_JS/25304.js
@@ -1,9 +1,9 @@
 // let receipt = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n')
 
-let receipt = ['250000', '4', '20000 5', '30000 2', '10000 6', '5000 8'];
+let receipt = ['260000', '4', '20000 5', '30000 2', '10000 6', '5000 8'];
 let total = parseInt(receipt[0], 10);
 let p, c;
-receipt = receipt.splice(2);
+receipt.splice(0, 2);
 
 for (col of receipt) {
     [p, c] = col.split(' ').map(Number);
