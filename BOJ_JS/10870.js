d253f46c611eab8c18bf13039ebb39e676dd1e37
da-in <talown@naver.com>
2022-10-31 16:33:16 +0900

    :memo: Solve BOJ JS 10870
    Solve BOJ JS 10870

---
+++ BOJ_JS/10870.js
@@ -0,0 +1,16 @@
+// let n = require('fs').readFileSync('/dev/stdin').toString();
+// n = parseInt(n, 10);
+
+let n = 10;
+
+function fibonacci(n) {
+    if (n == 0) {
+        return 0;
+    } else if (n == 1) {
+        return 1;
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
+
+console.log(fibonacci(n));
