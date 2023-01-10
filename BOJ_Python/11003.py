c2e2abf5db25cc048b26466b45da73b8cc8c7287
da-in <talown@naver.com>
2023-01-10 22:08:51 +0900

    :memo: Solve BOJ 11003.py
    Solve BOJ 11003.py

---
+++ BOJ_Python/11003.py
@@ -0,0 +1,13 @@
+from collections import deque
+
+N, L = map(int, input().split())
+num = list(map(int, input().split()))
+deq = deque()
+
+for i in range(N):
+    while deq and deq[-1][0] > num[i]:
+        deq.pop()
+    deq.append((num[i], i))
+    if (deq[0][1] <= i-L):
+        deq.popleft()
+    print(deq[0][0], end=' ')
